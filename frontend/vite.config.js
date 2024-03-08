66f7456ab2bf2d52f5c69bed88a6600cab1635a5
Duy <truongvanduy09072002@gmail.com>
2024-03-08 09:01:54 +0700

    chore: config server port and api proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,5 +18,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
   }
 })

9c5d4cfdb361745faed71ccd5013dfa6da119c66
Duy <truongvanduy09072002@gmail.com>
2024-03-09 14:06:38 +0700

    feat (content pane): create content header

---
+++ frontend/src/components/ContentHeader.vue
@@ -0,0 +1,38 @@
+<script setup>
+import MdIconButton from '@/components/buttons/MdIconButton.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
+
+<template>
+  <div class="content-header">
+    <MdIconButton @click="router.go(-1)">arrow_back</MdIconButton>
+    <div class="container">
+      <h1 class="fs-3">
+        <slot></slot>
+      </h1>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.content-header {
+  position: relative;
+  display: flex;
+  align-items: center;
+  column-gap: 2rem;
+  container: container / inline-size;
+
+  /**=============================
+    65rem: 60rem (container width)
+          + 3rem (back button size)
+          + 2rem (column gap)
+  ===============================*/
+  @container container (width > 65rem) {
+    md-icon-button {
+      position: absolute;
+    }
+  }
+}
+</style>

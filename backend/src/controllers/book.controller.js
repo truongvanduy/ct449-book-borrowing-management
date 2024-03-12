c05e5b43ff194e12b7c9db434dda86f1ed69c038
Duy <truongvanduy09072002@gmail.com>
2024-03-13 00:14:27 +0700

    feat: implement book searching

--- backend/src/controllers/book.controller.js
+++ backend/src/controllers/book.controller.js
@@ -7,6 +7,7 @@ const bookController = {
     const bookService = new BookService(MongoDB.client);
     try {
       const [document] = await bookService.findOne(req.params.id);
+
       if (!document) {
         return next(new ApiError(404, 'Book not found'));
       }
@@ -20,11 +21,16 @@ const bookController = {
       );
     }
   },
-  findAll: async (_req, res, next) => {
+  findAll: async (req, res, next) => {
     let documents = [];
+    const { q } = req.query;
+
+    // Filter all books with title matching the search query
+    const filter = (q && { title: new RegExp(`.*${q}.*`, 'i') }) || {};
+
     try {
       const bookService = new BookService(MongoDB.client);
-      documents = await bookService.find();
+      documents = await bookService.find(filter);
       return res.json(documents);
     } catch (error) {
       return next(
--- frontend/src/views/BooksView.vue
+++ frontend/src/views/BooksView.vue
@@ -1,7 +1,12 @@
 <script setup>
-import { computed, onMounted, ref } from 'vue'
+import { computed, ref, watchEffect } from 'vue'
 import BookList from '@/components/cards/BookList.vue'
 import ThePagination from '@/components/ThePagination.vue'
+import bookService from '@/services/book.service'
+import { useRoute } from 'vue-router'
+
+// Search values
+const route = useRoute()
 
 // let items = bookSource
 const items = ref([])
@@ -18,15 +23,29 @@ const handlePageChange = (page) => {
   currentPage.value = page
 }
 
-onMounted(async () => {
-  const response = await fetch('http://localhost:3000/api/books')
-  items.value = await response.json()
+// Display book cards
+
+const loading = ref(true)
+
+watchEffect(async () => {
+  const filter = route.query || {}
+
+  loading.value = true
+
+  const response = await bookService.getAll(filter)
+  items.value = response
+
+  loading.value = false
 })
 </script>
 
 <template>
   <h1 class="fs-2">Thư viện</h1>
-
+  <md-linear-progress
+    v-if="loading"
+    class="auth-progress"
+    indeterminate
+  ></md-linear-progress>
   <ThePagination
     v-if="items.length"
     :itemCount="items.length"

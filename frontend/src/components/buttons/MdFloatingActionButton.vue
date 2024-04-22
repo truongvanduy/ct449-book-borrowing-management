833fd8b5efa8f6b42c4742e35502836e6e41bba1
Duy <truongvanduy09072002@gmail.com>
2024-04-22 23:22:25 +0700

    feat: implement FAB component

---
+++ frontend/src/components/buttons/MdFloatingActionButton.vue
@@ -0,0 +1,49 @@
+<script setup>
+import MdIcon from '../icons/MdIcon.vue'
+defineProps({
+  label: {
+    type: String,
+    default: ''
+  },
+  size: {
+    type: String,
+    default: 'medium'
+  },
+  isFixed: {
+    type: Boolean,
+    default: true
+  },
+  icon: {
+    type: String,
+    default: 'add'
+  },
+  variant: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <md-fab
+    :class="{ fab: isFixed }"
+    :size="size"
+    :aria-label="label"
+    :label="size === 'medium' ? label : ''"
+    :variant="variant"
+  >
+    <MdIcon
+      slot="icon"
+      :font-size="size === 'large' ? 2.25 : 1.5"
+      >{{ icon }}</MdIcon
+    >
+  </md-fab>
+</template>
+
+<style lang="scss" scoped>
+.fab {
+  position: fixed;
+  right: calc(2 * var(--content-margin));
+  bottom: calc(2 * var(--content-margin));
+}
+</style>
